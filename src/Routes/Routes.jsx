9a6aa61a626c427e7aaf11dc4a392020e314e0ec
shahharsh99 <37408128+shahharsh99@users.noreply.github.com>
2021-05-25 21:54:20 +0530

    Update Routes.jsx

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -7,36 +7,82 @@ import {
 import ForgetPassword from "../Components/Pages/ForgetPassword";
 import SignIn from "../Components/Pages/SignIn/SignIn";
 import AuthRoute from "./AuthRoute";
+import Dashboard from "../Components/Pages/Dashboard"
+import  AddEditCustomer from "../Components/Pages/CustomerManagement/addEdit"
+import CustomerManagement from "../Components/Pages/CustomerManagement";
 
-const AutheticateRoute = ({ component: Component, ...rest }) => {
-const authToken = localStorage.getItem("userAccessToken");
+import MasterData from "../Components/Pages/MasterData";
 
-return (
-    <Route
-    {...rest}
-    render={(props) =>
-        authToken ? <AuthRoute {...props} /> : <Redirect to="/login" />
-    }
-    />
-);
-};
+
+import ManageUnits from "../Components/Pages/ManageUnits";
+import { AddEditUnit } from "../Components/Pages/ManageUnits/addEdit";
+
+import RoleBasedManagement from "../Components/Pages/RoleBasedManagement";
+import { AddEditRoles } from "../Components/Pages/RoleBasedManagement/addEdit";
+import Logs from "../Components/Pages/Logs";
+import ChangePassword from "../Components/Pages/ChangePassword";
+
+// const AutheticateRoute = ({ component: Component, ...rest }) => {
+// const authToken = localStorage.getItem("userAccessToken");
+
+// // return (
+// //     <Route
+// //     {...rest}
+// //     render={(props) =>
+// //         authToken ? <AuthRoute {...props} /> : <Redirect to="/login" />
+// //     }
+// //     />
+// // );
+// };
 
 const Routes = () => {
     return (
-    <Router 
-    //   basename={process.env.PUBLIC_URL}
-      >
+//     <Router 
+//     //   basename={process.env.PUBLIC_URL}
+//       >
+//         <Switch>
+//             <Route  path="/forgetPassword" component={ForgetPassword}/>
+//             <Route exact name = "login" path="/login" component={SignIn}/>
+//             <AutheticateRoute
+//                 path="/"
+//                 name="Login"
+//                 render={(props) => <AuthRoute {...props} />}
+//             />
+
+//             <Route path="/" render={() => <Redirect to="/login" />} />
+//         </Switch>
+//     </Router>
         <Switch>
-            <Route  path="/forgetPassword" component={ForgetPassword}/>
+
+        {/* Change Password */}
+        <Route exact path="/changePassword" component={ChangePassword }/>
+
+         {/* Logs */}
+         <Route exact path="/logs" component={Logs}/>
+
+         {/* Role Management */}
+         <Route exact path="/addeditRole" component={AddEditRoles}/>
+        <Route exact path="/role" component={RoleBasedManagement}/>
+    
+        {/* Manage Units */}
+        <Route exact path="/addunit" component={AddEditUnit}/>
+        <Route exact path="/manageunit" component={ManageUnits}/>
+
+        {/* Customers Management */}
+        <Route exact path="/addCustomer" component={AddEditCustomer}/>
+        <Route exact path="/customer" component={CustomerManagement}/>
+
+        {/* Master Data */}
+        <Route exact path="/masterdata" component={MasterData}/>
+
+        {/* dashboard */}
+        <Route exact path="/dashboard" component={Dashboard}/>
+
+              <Route  path="/forgetPassword" component={ForgetPassword}/>
             <Route exact name = "login" path="/login" component={SignIn}/>
-            <AutheticateRoute
-                path="/"
-                name="Login"
-                render={(props) => <AuthRoute {...props} />}
-            />
 
             <Route path="/" render={() => <Redirect to="/login" />} />
-        </Switch>
-    </Router>)}
+    </Switch>
+)}
 
 export default Routes;
